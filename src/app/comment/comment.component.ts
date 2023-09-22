e6f78461f2170b84b2cd03a754155ff14539609e
Nathan Hadley <nathan.hadley@me.com>
2023-09-22 12:17:02 -0700

    improve variable names and add comments

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -18,6 +18,7 @@ interface Comment {
 export class CommentComponent {
   comments: Comment[] = [];
 
+  // Placeholder array of users
   users: User[] = [
     {'userID' : 1, 'name' : 'Kevin'},
     {'userID' : 2, 'name' : 'Jeff'},
@@ -27,12 +28,13 @@ export class CommentComponent {
     {'userID' : 6, 'name' : 'Kyle'},
   ];
 
+  // Template reference to the mention/user list popup
   @ViewChild('mentionList') mentionList: ElementRef | undefined;
 
-  selectedIndex: number = 0;
-  newComment = '';         // Model for the comment input field
-  showMentionList = false; // Flag to show/hide the mention list
-  filteredUsers: User[] = [];     // Filtered user list based on the @mention
+  selectedUserIndex: number = 0;
+  newCommentText = '';
+  showMentionList = false;
+  filteredUsers: User[] = []; // Filtered user list based on the @mention
 
   // Method to detect @mention in the comment input
   detectMention(event: KeyboardEvent) {
@@ -40,32 +42,16 @@ export class CommentComponent {
     const lastWord: string | undefined = input.split(" ").pop();
 
     if (lastWord?.startsWith('@')) {
+      // Do case-insensitive matching
       this.filteredUsers = this.users.filter(user => user.name.toLowerCase().startsWith(lastWord.substr(1).toLowerCase()));
       this.showMentionList = true;
     } else {
       this.showMentionList = false;
-      this.selectedIndex = 0;
+      this.selectedUserIndex = 0; // Reset index so that when popup reappears, selected user is at the top again
     }
   }
 
-  // Method to add the comment to the comments array
-  addComment() {
-    if (this.newComment) {
-      this.comments.push({
-        text: this.newComment,
-        timestamp: new Date().toString()
-      });
-
-      const mentionedUsers = this.users.filter(user => this.newComment.includes(`@${user.name}`));
-      mentionedUsers.forEach(user => {
-        alert(`Mentioned: ${user.name}`);
-      });
-
-      this.newComment = '';
-      this.showMentionList = false;
-    }
-  }
-
+  // Method to handle key events
   handleKeydown(event: KeyboardEvent) {
     if (this.showMentionList && this.mentionList) {
       const list = this.mentionList.nativeElement;
@@ -74,39 +60,39 @@ export class CommentComponent {
 
       // Enter key
       if (event.key === 'Enter') {
-        event.preventDefault();
-        this.selectUser(this.filteredUsers[this.selectedIndex].name);
+        event.preventDefault(); // Not really needed because pressing enter does NOT submit comment form
+        this.selectUser(this.filteredUsers[this.selectedUserIndex].name);
       }
 
       // Arrow down
       if (event.key === 'ArrowDown') {
-        event.preventDefault();
-        if (this.selectedIndex < this.filteredUsers.length - 1) {
-          this.selectedIndex++;
+        event.preventDefault(); // Prevent keying down in textbox
+        if (this.selectedUserIndex < this.filteredUsers.length - 1) {
+          this.selectedUserIndex++;
         } else {
-          this.selectedIndex = 0;
+          this.selectedUserIndex = 0;
           list.scrollTop = 0; // Reset scroll to top when moving from the last item to the first
         }
 
         // If the next item is below the visible area
-        if (items[this.selectedIndex].offsetTop + items[this.selectedIndex].clientHeight > list.scrollTop + list.clientHeight) {
-          list.scrollTop = items[this.selectedIndex].offsetTop + items[this.selectedIndex].clientHeight - list.clientHeight;
+        if (items[this.selectedUserIndex].offsetTop + items[this.selectedUserIndex].clientHeight > list.scrollTop + list.clientHeight) {
+          list.scrollTop = items[this.selectedUserIndex].offsetTop + items[this.selectedUserIndex].clientHeight - list.clientHeight;
         }
       }
 
       // Arrow up
       if (event.key === 'ArrowUp') {
-        event.preventDefault();
-        if (this.selectedIndex > 0) {
-          this.selectedIndex--;
+        event.preventDefault(); // Prevent keying down in textbox
+        if (this.selectedUserIndex > 0) {
+          this.selectedUserIndex--;
         } else {
-          this.selectedIndex = this.filteredUsers.length - 1;
+          this.selectedUserIndex = this.filteredUsers.length - 1;
           list.scrollTop = list.scrollHeight; // Scroll to the bottom when moving from the first item to the last
         }
 
         // If the previous item is above the visible area
-        if (items[this.selectedIndex].offsetTop < list.scrollTop) {
-          list.scrollTop = items[this.selectedIndex].offsetTop;
+        if (items[this.selectedUserIndex].offsetTop < list.scrollTop) {
+          list.scrollTop = items[this.selectedUserIndex].offsetTop;
         }
       }
     }
@@ -114,9 +100,31 @@ export class CommentComponent {
 
   // Method to select a user from the mention list
   selectUser(userName: string) {
-    this.newComment = this.newComment.replace(/@(\w+)?$/, `@${userName}`);
+    // Replace partially finished '@' string with full username
+    this.newCommentText = this.newCommentText.replace(/@(\w+)?$/, `@${userName}`);
     this.showMentionList = false;
-    this.selectedIndex = 0;
+    this.selectedUserIndex = 0;
+  }
+
+  // Method to add the comment to the comments array
+  addComment() {
+    if (this.newCommentText) {
+      this.comments.push({
+        text: this.newCommentText,
+        timestamp: new Date().toString()
+      });
+
+      // Grab all mentioned users
+      const mentionedUsers = this.users.filter(user => this.newCommentText.includes(`@${user.name}`));
+
+      // Alert each mentioned user
+      mentionedUsers.forEach(user => {
+        alert(`Mentioned: ${user.name}`); // Or send email/push notification
+      });
+
+      this.newCommentText = '';
+      this.showMentionList = false;
+    }
   }
 
 }
